2c719cffb0bc921c17067225c49cf5a89863d8a9
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-11 23:06:25 +1000

    Created variables for selecting html elements

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Note:
+// Queryselector only selects the first instance of an html element in a page
+// Unless you use: querySelectorAll
+
+// Organising elements
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// If its only one line, technically you don't need curly braces for a block under a for loop
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
