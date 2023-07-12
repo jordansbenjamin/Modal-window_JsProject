5431f830a2708156d3386d7409408e558120aa8e
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-12 20:20:05 +1000

    Added event listener for keydown events

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const openModal = function() {
-  console.log('Button clicked');
+const openModal = function () {
+  // console.log('Button clicked');
   // To remove the class (dont need the dot)
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-}
+};
 
-const closeModal = function() {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-}
+};
 
 // If its only one line, technically you don't need curly braces for a block under a for loop
 for (let i = 0; i < btnsOpenModal.length; i++)
@@ -30,3 +30,16 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 btnCloseModal.addEventListener('click', closeModal);
 // Closing the modal outside on the overlay
 overlay.addEventListener('click', closeModal);
+
+// Listening for events globally, usually used for keyboard presses
+document.addEventListener('keydown', function (event) {
+  // if (event.key === 'Escape') {
+  //   if (!modal.classList.contains('hidden')) {
+  //     closeModal();
+  //   }
+  // }
+  // Using && to shorten the above
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
