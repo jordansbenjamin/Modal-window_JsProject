7195085f5b4648e7303ebe3a82c91cba90dd1a95
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-12 19:58:48 +1000

    Added event handlers for opening and closing the modal

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function() {
+  console.log('Button clicked');
+  // To remove the class (dont need the dot)
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closeModal = function() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
 // If its only one line, technically you don't need curly braces for a block under a for loop
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// Closing the modal with the x (close) btn
+btnCloseModal.addEventListener('click', closeModal);
+// Closing the modal outside on the overlay
+overlay.addEventListener('click', closeModal);
